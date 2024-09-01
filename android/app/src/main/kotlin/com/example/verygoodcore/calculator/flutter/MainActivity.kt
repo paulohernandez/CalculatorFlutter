81b58d75fffdc4ef383fd0f9487f8fc707d53209
Paulo13 <107038071+Tomador13@users.noreply.github.com>
2024-09-01 17:01:33 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/calculator/flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.calculator_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:calculator_flutter/app/app.dart';
+import 'package:calculator_flutter/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
